6491c79fa559c8a8ad524a719a44991f7d641f53
Barry-Thomas-Paul: Moss <vibrationoflife@protonmail.com>
2024-06-26 13:53:17 +0000

    dialog mb testing

---
+++ oxt/python/scripts/win_mb.py
@@ -0,0 +1,28 @@
+from __future__ import annotations
+from typing import TYPE_CHECKING
+import uno
+
+if TYPE_CHECKING:
+    from com.sun.star.script.provider import XScriptContext
+    from com.sun.star.awt import ActionEvent
+
+    from ...pythonpath.libre_pythonista_lib.log.log_inst import LogInst
+    from ...pythonpath.libre_pythonista_lib.dialog.py.dialog_mb import DialogMb
+
+    XSCRIPTCONTEXT: XScriptContext
+else:
+    from libre_pythonista_lib.log.log_inst import LogInst
+    from libre_pythonista_lib.dialog.py.dialog_mb import DialogMb
+
+
+def show_win_mb(*args):
+    """
+    Handle the button action event.
+    """
+
+    log = LogInst()
+    log.debug("show_win_mb debug")
+    try:
+        mb = DialogMb()
+    except Exception:
+        log.error("show_win_mb: Error", exc_info=True)
