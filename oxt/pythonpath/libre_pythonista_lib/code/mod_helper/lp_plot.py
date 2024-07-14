3a3bd404241722e3a4667e3093a02051b660d66f
Barry-Thomas-Paul: Moss <vibrationoflife@protonmail.com>
2024-07-14 17:07:06 -0400

    fix for plt.show()

--- oxt/pythonpath/libre_pythonista_lib/code/mod_helper/lp_plot.py
+++ oxt/pythonpath/libre_pythonista_lib/code/mod_helper/lp_plot.py
@@ -20,7 +20,14 @@ def _lp_plt_show_prefix_function(function: Any, pre_function: Any):
     @functools.wraps(function)
     def run(*args, **kwargs):
         pre_function(*args, **kwargs)
-        return function(*args, **kwargs)
+        # in theory the real plot.show() can be called here.
+        # However, this would run the real plot.show() which may popup an window and
+        # event crash LibreOffice. So, we just return None here.
+        # I LibreOffice Flatpak, this seem to make no difference.
+        # Because the image is saved to a file, it can be loaded in LibreOffice.
+        #
+        # return function(*args, **kwargs)
+        return None
 
     return run
 
@@ -28,8 +35,8 @@ def _lp_plt_show_prefix_function(function: Any, pre_function: Any):
 def _custom_plt_show(*args, **kwargs):
     # Your own code here that will be run before
     global LAST_LP_RESULT
-    log = LogInst()
-    log.info("Custom Plot Method")
+    # log = LogInst()
+    # log.debug("Custom Plot Method")
     s = "plt_" + OooDevGenUtil.generate_random_hex_string(12) + ".svg"
     pth = Lo.tmp_dir / s
     plt.savefig(str(pth), format="svg")
