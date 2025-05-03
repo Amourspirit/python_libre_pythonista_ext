96e02e527c914b6bd8af2b2bf32385d1f4851a1e
Barry-Thomas-Paul: Moss <vibrationoflife@protonmail.com>
2025-05-03 15:38:19 -0600

    Remove unused imports and methods from QryRowsColsPdSeries for cleaner code

--- oxt/pythonpath/libre_pythonista_lib/cq/qry/calc/sheet/cell/array/qry_rows_cols_pd_series.py
+++ oxt/pythonpath/libre_pythonista_lib/cq/qry/calc/sheet/cell/array/qry_rows_cols_pd_series.py
@@ -1,23 +1,15 @@
 from __future__ import annotations
 
-from typing import cast, List, TYPE_CHECKING, Iterable
+from typing import cast, List, TYPE_CHECKING
 
 if TYPE_CHECKING:
     from oxt.pythonpath.libre_pythonista_lib.doc.calc.doc.sheet.cell.code.module_state_item import ModuleStateItem
     from oxt.pythonpath.libre_pythonista_lib.cq.qry.calc.sheet.cell.array.qry_rows_cols_tbl import QryRowsColsTbl
-    from oxt.pythonpath.libre_pythonista_lib.cq.qry.calc.sheet.cell.prop.qry_pyc_rule import QryPycRule
-    from oxt.pythonpath.libre_pythonista_lib.cq.qry.calc.sheet.cell.rule_value.qry_value import QryValue
-    from oxt.pythonpath.libre_pythonista_lib.kind.rule_name_kind import RuleNameKind
     from oxt.pythonpath.libre_pythonista_lib.utils.custom_ext import override
-    from oxt.pythonpath.libre_pythonista_lib.utils.result import Result
 else:
     from libre_pythonista_lib.doc.calc.doc.sheet.cell.code.module_state_item import ModuleStateItem
     from libre_pythonista_lib.cq.qry.calc.sheet.cell.array.qry_rows_cols_tbl import QryRowsColsTbl
-    from libre_pythonista_lib.cq.qry.calc.sheet.cell.prop.qry_pyc_rule import QryPycRule
-    from libre_pythonista_lib.cq.qry.calc.sheet.cell.rule_value.qry_value import QryValue
-    from libre_pythonista_lib.kind.rule_name_kind import RuleNameKind
     from libre_pythonista_lib.utils.custom_ext import override
-    from libre_pythonista_lib.utils.result import Result
 
 
 class QryRowsColsPdSeries(QryRowsColsTbl):
@@ -28,25 +20,6 @@ class QryRowsColsPdSeries(QryRowsColsTbl):
         cell (CalcCell): The cell to query
         mod (PyModule, None): Optional Python module. If None, will be queried using QryPyModuleDefault
     """
-
-    def _qry_ctl_rule_name_kind(self) -> RuleNameKind:
-        """Queries the rule name kind for the cell's control"""
-        qry = QryPycRule(self.cell)
-        result = self._execute_qry(qry)
-        if Result.is_success(result):
-            self.log.debug("qry_ctl_rule_name_kind_result: %s for cell: %s", result.data, self.cell.cell_obj)
-            return result.data
-        raise result.error
-
-    def _qry_value(self) -> Iterable[Iterable[object]]:
-        """Queries the value of the cell"""
-        rule_kind = self._qry_ctl_rule_name_kind()
-        state = self._qry_module_state()
-        qry = QryValue(cell=self.cell, rule_kind=rule_kind, data=state.dd_data)
-        result = self._execute_qry(qry)
-        if Result.is_success(result):
-            return result.data
-        raise result.error
 
     @override
     def _get_rows_cols(self, module_item: ModuleStateItem) -> List[int]:
@@ -59,7 +32,8 @@ class QryRowsColsPdSeries(QryRowsColsTbl):
         if TYPE_CHECKING:
             import pandas as pd
 
-        s = cast("pd.Series", self._qry_value())
+        # s = cast("pd.Series", self._qry_value())
+        s = cast("pd.Series", module_item.dd_data.data)
         series_len = len(s)
         if not series_len:
             return [0, 0]
